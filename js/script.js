c1cfc2817933e462632bb849229de7ac9d289cd6
Kenedy <kenedy@kenedy.dev>
2024-12-02 15:40:09 -0300

    remove a funcao de ocultar o headar e adiciona o icone de fechar menu

--- js/script.js
+++ js/script.js
@@ -1,46 +1,33 @@
-let lastScrollPosition = 0;
-const header = document.querySelector('.header');
 const menuToggle = document.querySelector('.header__menu-toggle');
 const menu = document.querySelector('.header__menu');
+const menuIcons = document.querySelectorAll('.header__menu-icon');
 const menuLinks = document.querySelectorAll('.header__menu-link');
-const SCROLL_THRESHOLD = 14;
-let isScrolling = false;
 
-window.addEventListener('scroll', () => {
-    if (!isScrolling) {
-        window.requestAnimationFrame(() => {
-            handleScroll();
-            isScrolling = false;
-        });
-        isScrolling = true;
+function updateMenuIcon() {
+    if (menu.classList.contains('header__menu--show')) {
+        menuIcons[0].classList.add('header__menu-icon--hide');
+        menuIcons[1].classList.remove('header__menu-icon--hide');
+    } else {
+        menuIcons[1].classList.add('header__menu-icon--hide');
+        menuIcons[0].classList.remove('header__menu-icon--hide');
     }
-}, { passive: true });
-
-function handleScroll() {
-    const currentScrollPosition = window.scrollY;
-    if (currentScrollPosition - lastScrollPosition > SCROLL_THRESHOLD) {
-        header.classList.add('header--hidden');
-        menu.classList.remove('header__menu--show');
-    } else if (currentScrollPosition < lastScrollPosition) {
-        header.classList.remove('header--hidden');
-    }
-
-    lastScrollPosition = currentScrollPosition;
 }
 
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('header__menu--show');
+    updateMenuIcon();
 });
 
 document.addEventListener('click', (event) => {
     if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
         menu.classList.remove('header__menu--show');
+        updateMenuIcon();
     }
 });
 
 menuLinks.forEach((link) => {
     link.addEventListener('click', () => {
         menu.classList.remove('header__menu--show');
-        header.classList.add('header--hidden');
+        updateMenuIcon();
     });
 });
