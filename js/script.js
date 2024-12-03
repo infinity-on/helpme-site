39a5ecdd2562a84fbd6c0b66e3e67f01bd5fdd6f
Kenedy <kenedy@kenedy.dev>
2024-12-03 11:47:45 -0300

    atualiza o header e melhora a navegacao
    - define o primeiro item do menu como ativo no html
    - comenta o item `Sobre nos` no html
    - adiciona estilos para o item ativo do menu no css
    - adiciona funcao para scroll ate a posicao do item clicado do menu no js
    - adiciona funcao para ativar o item do menu correspondente a secao presente na viewport no js
    - refatora a funcao `updateMenuIcon` para um codigo limpo no js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
 const menuToggle = document.querySelector('.header__menu-toggle');
 const menu = document.querySelector('.header__menu');
-const menuIcons = document.querySelectorAll('.header__menu-icon');
+const [menuIconOpen, menuIconClose] = document.querySelectorAll('.header__menu-icon');
 const menuLinks = document.querySelectorAll('.header__menu-link');
+const sections = document.querySelectorAll('section');
+let lastActivedMenuLink = document.querySelector('.header__menu-item--active .header__menu-link');
+let notExecuteIntersectionObserver = false;
+let notExecuteIntersectionObserverTimeout = null;
+
+const observer = new IntersectionObserver((entries) => {
+    if (notExecuteIntersectionObserver) return;
+
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const link = document.querySelector(`.header__menu-link[href="#${entry.target.id}"]`);
+            if (lastActivedMenuLink) {
+                lastActivedMenuLink.parentElement.classList.remove('header__menu-item--active');
+            }
+            link.parentElement.classList.add('header__menu-item--active');
+            lastActivedMenuLink = link;
+        }
+
+    });
+}, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+});
+
+sections.forEach(section => observer.observe(section));
 
 function updateMenuIcon() {
-    if (menu.classList.contains('header__menu--show')) {
-        menuIcons[0].classList.add('header__menu-icon--hide');
-        menuIcons[1].classList.remove('header__menu-icon--hide');
-    } else {
-        menuIcons[1].classList.add('header__menu-icon--hide');
-        menuIcons[0].classList.remove('header__menu-icon--hide');
-    }
+    const isMenuVisible = menu.classList.contains('header__menu--show');
+    menuIconOpen.classList.toggle('header__menu-icon--hide', isMenuVisible);
+    menuIconClose.classList.toggle('header__menu-icon--hide', !isMenuVisible);
 }
 
 menuToggle.addEventListener('click', () => {
@@ -26,8 +48,36 @@ document.addEventListener('click', (event) => {
 });
 
 menuLinks.forEach((link) => {
-    link.addEventListener('click', () => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
         menu.classList.remove('header__menu--show');
         updateMenuIcon();
+
+        const href = link.getAttribute('href');
+        const targetSection = document.querySelector(href);
+        if (targetSection) {
+            const headerHeight = document.querySelector('.header').offsetHeight / 2;
+            const sectionPosition = targetSection.getBoundingClientRect().top + window.scrollY;
+
+            window.scrollTo({
+                top: sectionPosition - headerHeight,
+                behavior: 'smooth',
+            });
+
+            if (lastActivedMenuLink) {
+                lastActivedMenuLink.parentElement.classList.remove('header__menu-item--active');
+            }
+            link.parentElement.classList.add('header__menu-item--active');
+            lastActivedMenuLink = link;
+            notExecuteIntersectionObserver = true;
+
+            if (notExecuteIntersectionObserverTimeout) {
+                clearTimeout(notExecuteIntersectionObserverTimeout);
+            }
+
+            notExecuteIntersectionObserverTimeout = setTimeout(() => {
+                notExecuteIntersectionObserver = false;
+            }, 1000);
+        }
     });
 });
